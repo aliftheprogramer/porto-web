00c2f618ac2619ad0088ae03b950cb9dbcef0313
aliftheprogramer <alif.5220411242@student.uty.ac.id>
2025-03-29 21:46:06 +0700

    kelasr

--- src/app.jsx
+++ src/app.jsx
@@ -4,21 +4,20 @@ import Home from "./homesection/home";
 import About from "./aboutscreen/about";
 import Project from "./projectscreen/project";
 import Contact from "./contactsctreen/contact";
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <Router>
+        <Router basename="/aliftheprogramer.github.io">
             <NavBar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/Project" element={<Project />} />
+                <Route path="/project" element={<Project />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
         </Router>
-    )
+    );
 }
 
 export default App;
